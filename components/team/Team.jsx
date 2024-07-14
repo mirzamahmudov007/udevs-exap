6d08a988f8da7decdf0bedf6ac3754097fe3851c
mirzamahmudov007 <gayratjonuzbek07@gmail.com>
2024-07-14 12:12:35 +0500

    updated

--- components/team/Team.jsx
+++ components/team/Team.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
 import "./../../styles/sass/team.scss";
 
 const Team = () => {
+    const [count, setCount] = useState(1);
+
+    useEffect(() => {
+        if (count < 100) {
+            const timer = setInterval(() => {
+                setCount(prevCount => prevCount + 1);
+            }, 50);
+            return () => clearInterval(timer);
+        }
+    }, [count]);
+
+
     return (
         <div id="command" className="team">
             <div className="container">
@@ -11,7 +24,7 @@ const Team = () => {
                             For each project, we form a team that includes a project manager, business analyst, UI / UX designer, DevOps, QA engineer, backend and front-end developers.
                         </p>
                         <p className="team__left__count">
-                            100+
+                            {count}+
                         </p>
                         <p className="team__left__subtext">
                             Dedicated team
